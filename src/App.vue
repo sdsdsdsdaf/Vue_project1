37c5136699c5ff51670d67fed564ac67a9765d55
Kim DongChan <kdch6686@gmail.com>
2022-12-24 00:31:32 +0900

    Data Binding

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <!--{{데이터}}-->
+    <h4>XX 원룸</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>XX 원룸</h4>
+    <p>{{ price2 }} 만원</p> 
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
 
 export default {
   name: 'App',
+  //데이터 바인딩: JS데이터를 HTML에 (데이터 바인딩: object 자료를 html 안에)
+  data(){
+    return {
+      //이 return 괄호 안에 js데이터 집어넣어야 함 (TO DO 그렇다면 함수 식으로 만들어 다른 함수 return 가능>?)
+      price1: 60, //Object 자료형 인덱스: 값
+      price2: 70
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
