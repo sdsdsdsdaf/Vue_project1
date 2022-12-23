7014816c7dbe79377b70bab0428a727568411acd
Kim DongChan <kdch6686@gmail.com>
2022-12-24 01:44:48 +0900

    Data Binding

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,14 @@
 
   <div>
     <!--{{데이터}}-->
-    <h4>XX 원룸</h4>
+    <!--: "안에 있는 데이터"-->
+    <h4 :style="스타일">{{ products[0] }}</h4>
+    <p>50 만원</p>
+
+    <h4>{{ products[1] }}</h4>
     <p>{{ price1 }} 만원</p>
-  </div>
-  <div>
-    <h4>XX 원룸</h4>
+    
+    <h4>{{ products[2] }}</h4>
     <p>{{ price2 }} 만원</p> 
   </div>
   
@@ -23,8 +26,10 @@ export default {
   data(){
     return {
       //이 return 괄호 안에 js데이터 집어넣어야 함 (TO DO 그렇다면 함수 식으로 만들어 다른 함수 return 가능>?)
-      price1: 60, //Object 자료형 인덱스: 값
-      price2: 70
+      price1: 80, //Object 자료형 인덱스: 값
+      price2: 70,
+      스타일: 'color: blue',
+      products: ['역삼동 원룸', '천호동원룸', '마포구 원룸']
     }
   },
   components: {
