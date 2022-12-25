a842e9ace01b3d40f0636517c2627ba8761d6faf
Kim DongChan <kdch6686@gmail.com>
2022-12-25 17:31:05 +0900

    V-for iteration

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
 
-  <div>
+  <div class="menu">
+    <!--for = "(값, 인덱스) vue 반복문 특징"-->
+    <a v-for="(value, index) in menus" v-bind:key="index">{{ value }}</a> 
+  </div>
+
+  <div v-for = "(value, index) in products" v-bind:key="index">
     <!--{{데이터}}-->
     <!--: "안에 있는 데이터"-->
-    <h4 :style="스타일">{{ products[0] }}</h4>
-    <p>50 만원</p>
-
-    <h4>{{ products[1] }}</h4>
-    <p>{{ price1 }} 만원</p>
-    
-    <h4>{{ products[2] }}</h4>
-    <p>{{ price2 }} 만원</p> 
+    <h4 :style="스타일">{{ value }}</h4>
+    <p>{{prices[index]}} 만원</p>
   </div>
   
 </template>
@@ -26,10 +24,10 @@ export default {
   data(){
     return {
       //이 return 괄호 안에 js데이터 집어넣어야 함 (TO DO 그렇다면 함수 식으로 만들어 다른 함수 return 가능>?)
-      price1: 80, //Object 자료형 인덱스: 값
-      price2: 70,
+      prices: [50, 80, 70], //Object 자료형 인덱스: 값
       스타일: 'color: blue',
-      products: ['역삼동 원룸', '천호동원룸', '마포구 원룸']
+      products: ['역삼동 원룸', '천호동원룸', '마포구 원룸'],
+      menus: ['Home', 'Shop', 'About']
     }
   },
   components: {
@@ -45,6 +43,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+
+.menu{
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a{
+  color: white;
+  padding: 10px;
 }
 </style>
